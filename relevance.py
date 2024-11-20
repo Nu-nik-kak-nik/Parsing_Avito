0a9b734d882e2a35879f5fba7f11993240fde58f
Nu-nik-kak-nik <lisicynaroslav003@gmail.com>
2024-11-20 21:51:12 +0700

    fifth commit (the relevance works)

--- relevance.py
+++ relevance.py
@@ -3,36 +3,25 @@ import variables
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 logger = variables.setup_logger()
 
 
 def check_ad_status(link: str | None, driver) -> bool | None:
+    try:
+        driver.get(link)
+        WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.TAG_NAME, "body")))
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        has_data_marker = any(item.has_attr('data-marker') and item['data-marker'] == "item-view/closed-warning" for item in soup.find_all())
+        return has_data_marker
 
-    # try:
-    #     options = Options()
-    #     options.add_argument("--headless")
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get(link)
-    #     driver.implicitly_wait(5)
-
-    #     if driver.find_elements(By.XPATH, "//div[@class='item-view-error__title']"):
-    #         driver.quit()
-    #         return False
-
-    #     else:
-    #         driver.quit()
-    #         return True
-
-    # except Exception as e:
-    #     logger.error(f"error checking link {link}: {e}", exc_info=True)
-    #     return False
-
-    if int(link) < 10:
-        return False
-    else:
-        return True
+    except Exception as e:
+        print(f"Error checking URL {link}: {e}")
+        return None
 
 
 def update_avito_links_file(final_file: str) -> None:
@@ -45,9 +34,10 @@ def update_avito_links_file(final_file: str) -> None:
 
     try:
         for link in links:
-            status = check_ad_status(link, driver)
-            if status is False:
-                valid_links.add(link)
+            if link:
+                status = check_ad_status(link, driver)
+                if status is False:
+                    valid_links.add(link)
 
         variables.write_to_file(valid_links, final_file, overwrite=True, indents=False)
         logger.info(f"File {final_file} updated. Number of valid links: {len(valid_links)}")
